5719800d4cb1cab2411e8c51ca80a629f0ed40de
Andrew <andrew@aguest.me>
2026-01-11 19:14:31 -0600

    Remove devtools import

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
-import devtools from 'solid-devtools/vite';
+
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin()],
+  plugins: [solidPlugin()],
   server: {
     port: 3000,
   },
